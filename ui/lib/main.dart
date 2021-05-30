9887f51d317f7d2aa2d69e23b9f9b1daf07f182d
collbreno <collbreno@dcc.ufrj.br>
2021-05-30 01:12:26 -0300

    Creating demo

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
-import 'package:business/business.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
 import 'package:ui/screens/home_screen.dart';
 
 void main() {
@@ -12,28 +9,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final store = Store<AppState>(
-      AppReducer(),
-      middleware: [],
-      initialState: AppState.initialState(),
-    );
-    return StoreProvider<AppState>(
-      store: store,
-      child: ThemeProvider(
-        initTheme: ThemeData(
-          primarySwatch: Colors.blue,
-          accentColor: Colors.blue[800],
-          brightness: Brightness.light,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        builder: (context, theme) {
-          return MaterialApp(
-            title: 'Extract',
-            theme: theme,
-            home: HomeScreen(),
-          );
-        },
+    return ThemeProvider(
+      initTheme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.blue[800],
+        brightness: Brightness.light,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      builder: (context, theme) {
+        return MaterialApp(
+          title: 'Extract',
+          theme: theme,
+          home: HomeScreen(),
+        );
+      },
     );
   }
 }
